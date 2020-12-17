f7755942b43dde3f96407f9a4f68fab5d319fdd1
Archibald Neil MacDonald <FortOyer@hotmail.co.uk>
2020-12-17 03:22:33 +0000

    initial commit.

---
+++ Assets/GoblinFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinFactory: MonoBehaviour {
+  public GameObject enemy;
+
+  public float goblin_timer = 1;
+  private static int enemy_count = 0;
+  private float time_to_next_goblin;
+
+  // Start is called before the first frame update
+  void Start() {
+    time_to_next_goblin = goblin_timer;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    time_to_next_goblin -= Time.deltaTime;
+    if (time_to_next_goblin <= 0) {
+      time_to_next_goblin = goblin_timer + Random.Range(0, 4);
+      Instantiate(enemy, transform.position, Quaternion.identity);
+      enemy_count += 1;
+    }
+  }
+}
---
+++ Assets/GoblinRun.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoblinRun: MonoBehaviour {
+  enum State {
+    Running,
+    Flying,
+    Falling
+  }
+
+  public float wanderRadius = 25;
+
+  private UnityEngine.AI.NavMeshAgent agent;
+
+  private float timer;
+
+  private Animator anim;
+
+  private State state;
+
+  private float current_y;
+
+  void Start() {
+    agent = GetComponent < UnityEngine.AI.NavMeshAgent > ();
+    agent.destination = RandomGoal(transform.position, wanderRadius, -1);
+
+    anim = GetComponent < Animator > ();
+    state = State.Running;
+    current_y = transform.position.y;
+  }
+
+  void Update() {
+    if (state == State.Flying) {
+      transform.position += new Vector3(0, 0.2f, 0);
+
+      if (transform.position.y - current_y > 15) {
+        transform.position = new Vector3(16, 20, 2);
+        state = State.Falling;
+      }
+    }
+    else if (state == State.Falling) {
+      transform.position -= new Vector3(0, 0.2f, 0);
+      if (transform.position.y - current_y < 0) {
+        Destroy(gameObject);
+      }
+    }
+    else if (GetComponent < Collider > ().bounds.Contains(agent.destination)) {
+      agent.destination = RandomGoal(transform.position, wanderRadius, -1);
+    }
+  }
+
+  void OnMouseDown() {
+    state = State.Flying;
+    Destroy(agent);
+    int n = Random.Range(0, 2);
+    if (n == 0) {
+      anim.SetInteger("death", 1);
+    } else {
+      anim.SetInteger("death", 2);
+    }
+  }
+
+  public Vector3 RandomGoal(Vector3 origin, float dist, int layermask) {
+    Vector2 randDirection2D = Random.insideUnitCircle * dist;
+    Vector3 randDirection = new Vector3(randDirection2D.x, 0, randDirection2D.y);
+
+    randDirection += new Vector3(9, origin.y, -6);
+
+    UnityEngine.AI.NavMeshHit navHit;
+
+    UnityEngine.AI.NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+    return transform.TransformPoint(navHit.position);
+  }
+}
